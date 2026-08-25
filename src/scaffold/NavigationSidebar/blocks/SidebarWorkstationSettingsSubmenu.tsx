b2642ad5c09e279ae15e75b7eccef435eeab58fc
Harry19081 <20519290+Harry19081@users.noreply.github.com>
2026-08-25 15:41:41 +0800

    fix(dropdowns): align breadcrumb and menu spacing

--- src/scaffold/NavigationSidebar/blocks/SidebarWorkstationSettingsSubmenu.tsx
+++ src/scaffold/NavigationSidebar/blocks/SidebarWorkstationSettingsSubmenu.tsx
@@ -132,14 +132,14 @@ export const SidebarWorkstationSettingsSubmenu: React.FC<SidebarWorkstationSetti
             options={chatPositionOptions}
             onChange={handleStationChatPositionChange}
           />
-          <div className={DROPDOWN_CLASSES.menuSeparator} />
+          <div className={DROPDOWN_CLASSES.menuSeparatorInset} />
           <SelectionRow<ChatPanelPosition>
             label={t("layoutSettings.agentStation")}
             value={agentChatPosition}
             options={chatPositionOptions}
             onChange={setAgentChatPosition}
           />
-          <div className={DROPDOWN_CLASSES.menuSeparator} />
+          <div className={DROPDOWN_CLASSES.menuSeparatorInset} />
           <SwitchControlRow
             label={t("layoutSettings.chatPanelPagination")}
             checked={chatTurnPaginationEnabled}
@@ -154,7 +154,7 @@ export const SidebarWorkstationSettingsSubmenu: React.FC<SidebarWorkstationSetti
             options={chatPositionOptions}
             onChange={setLayoutModePersist}
           />
-          <div className={DROPDOWN_CLASSES.menuSeparator} />
+          <div className={DROPDOWN_CLASSES.menuSeparatorInset} />
           <SelectionRow<ModelPickerStyle>
             label={t("layoutSettings.modelPickerStyle")}
             value={modelPickerStyle}
